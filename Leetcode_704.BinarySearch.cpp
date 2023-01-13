cacebcd8a22c454d0df021b5a8d184c6a24f3002
siangting <s0978592737@gmail.com>
2023-01-13 16:57:20 +0800

    [LeetCode] 704. Binary Search

---
+++ Leetcode_704.BinarySearch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        int mid=0;
+
+        while(left<=right){
+            mid=int (left+right)/2;
+
+            if(target>nums[mid]){
+                left=mid+1;
+            }
+            else if(target<nums[mid]){
+                right=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
